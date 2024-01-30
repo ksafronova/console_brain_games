f3a7dc41f13d828b6b957853bcd202d65b13fb4c
Kseniia Safronova <safronovaksusha96@gmail.com>
2024-01-30 10:44:19 +0400

    lint fixes

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
-import evenGame from "../src/even-game.js";
 import readlineSync from 'readline-sync';
+import evenGame from '../src/even-game.js';
 
 console.log('Welcome to the Brain Games!');
 const userName = readlineSync.question('May I have your name?');
 console.log(`Hello, ${userName}!`);
 console.log('Answer "yes" if the number is even, otherwise answer "no');
 evenGame(3, userName);
-console.log(`Congratulations, ${userName}`) ;
+console.log(`Congratulations, ${userName}`);
--- src/is-even.js
+++ src/is-even.js
@@ -1,3 +1,3 @@
 export default function isEven(number) {
-	return number % 2 === 0 ? 'yes' : 'no'
+  return number % 2 === 0 ? 'yes' : 'no';
 }
